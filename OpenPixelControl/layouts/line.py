fd798a212cf5895c373736a6053a2c32803327d1
Carl Hill-Popper <chillpop@gmail.com>
2014-04-12 16:51:28 -0500

    Updated OPC simulator layout to see better around the axes.

--- OpenPixelControl/layouts/line.py
+++ OpenPixelControl/layouts/line.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
-spacing = 0.11  # m
+x_spacing = 0.11  # m
+y_value = 0
+z_value = -1
 num_points = 120
 lines = []
 for c in range(-(num_points/2), (num_points/2)) :
     lines.append('  {"point": [%.2f, %.2f, %.2f]}' %
-                 (c*spacing, 0, spacing))
+                 (c*x_spacing, y_value, z_value))
 print '[\n' + ',\n'.join(lines) + '\n]'
