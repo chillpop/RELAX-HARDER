3efc2f6e54e2a8f895f94834bbb6bdbd86554567
Carl Hill-Popper <chillpop@gmail.com>
2014-04-27 22:29:19 -0500

    Calculate the percentage to fill as a function of the difference between two values and time
    At the beginning, the winning values needs a large delta to the losing value.  Over time, that "delta needed to win" decreases to a lower bound to make winning easier.

--- relax_harder.py
+++ relax_harder.py
@@ -14,23 +14,48 @@ from threads import HeadsetThread
 # Note: on OS X, BluetoothHeadset will not work
 from OpenPixelControl import opc
 import math
+import numpy
 
 NUM_PIXELS = 120
 
 PLAYER_ONE_ADDRESS = '74:E5:43:BE:39:71'
 PLAYER_ONE_CHAR = '*'
+PLAYER_ONE_COLOR = 255, 0, 0
 PLAYER_TWO_ADDRESS = '74:E5:43:B1:96:E0'
 PLAYER_TWO_CHAR = '^'
+PLAYER_TWO_COLOR = 0, 0, 255
 
-def bar_from_datapoints(point1, point2):
-    m1 = point1.meditation
-    m2 = point2.meditation
-    span = max(m1, m2) - min(m1, m2)
-    if span == 0 or (m1 < 0.1 and m2 < 0.1):
-        return NUM_PIXELS * ' '
-    percentage = m1 / (m1 + m2)
+MAX_DELTA = 1.0
+MIN_DELTA = 0.05
+CHANGE_IN_DELTA_PER_SECOND = 0.01
+ELAPSED_STARTUP_TIME = 2.0
+TIME_AT_MAX = 2.0
 
-    p1_chars = int(math.ceil(percentage * NUM_PIXELS)) * PLAYER_ONE_CHAR
+start_time = time.time()
+
+def delta_needed_to_win():
+    """delta needed to win starts at a max value and slowly goes down over time
+    """
+    elapsed_time = time.time() - (start_time + ELAPSED_STARTUP_TIME)
+    elapsed_time = max(elapsed_time, 0)
+    return max(MAX_DELTA - (CHANGE_IN_DELTA_PER_SECOND * elapsed_time), MIN_DELTA)
+
+def percentage_from_values(m1, m2):
+    percentage = 0.5
+    #don't do anything unless we have both values
+    if m1 > 0.001 and m2 > 0.001:
+        delta = m1 - m2
+        #take the difference between the values
+        #(delta / delta_needed_to_win) will be centered around zero
+        percentage = delta / delta_needed_to_win()
+        #make sure we don't go out of bounds [-1, 1]
+        percentage = numpy.clip(percentage, -1.0, 1.0)
+        #transform that into [0, 1]
+        percentage = (1 + percentage) * 0.5
+    return percentage
+
+def bar_from_percentage(percentage):
+    p1_chars = int(round(percentage * NUM_PIXELS)) * PLAYER_ONE_CHAR
     p2_chars = (NUM_PIXELS - len(p1_chars)) * PLAYER_TWO_CHAR
     bar = p1_chars + p2_chars
     return bar
@@ -39,12 +64,11 @@ def pixels_from_bar(bar):
     pixels = []
     for c in bar:
         # off unless we see a character we know
-        rgb = [0, 0, 0]
+        rgb = 0, 0, 0
         if c == PLAYER_ONE_CHAR:
-            rgb = [255, 0, 0]
+            rgb = PLAYER_ONE_COLOR
         elif c == PLAYER_TWO_CHAR:
-            rgb = [0, 0, 255]
-        rgb = tuple(rgb)
+            rgb = PLAYER_TWO_COLOR
         pixels.append(rgb)
     return pixels
 
@@ -80,8 +104,8 @@ if __name__ == '__main__':
         shared_params.targetFrameRate = 100.0; # let's go for it
     shared_params.debug = False
 
-    player1 = FakeHeadset(random_data=True) if test else BluetoothHeadset(PLAYER_ONE_ADDRESS)
-    player2 = FakeHeadset(random_data=True) if test else BluetoothHeadset(PLAYER_TWO_ADDRESS)
+    player1 = FakeHeadset(random_data = True) if test else BluetoothHeadset(PLAYER_ONE_ADDRESS)
+    player2 = FakeHeadset(random_data = True) if test else BluetoothHeadset(PLAYER_TWO_ADDRESS)
 
     threads = [
         HeadsetThread(shared_params, player1),        
@@ -94,7 +118,9 @@ if __name__ == '__main__':
     time.sleep(0.05)
 
     fps = 60.0
-    frames_to_average = 5
+
+    win_time = None
+    potential_winner = None
 
     point1 = None
     point2 = None
@@ -106,17 +132,33 @@ if __name__ == '__main__':
         point2 = shared_params.eeg2
       
         if point1 != None and point2 != None:
-            def delta(p, point, steps=fps):
+            def delta(p, point, steps = fps):
                 if p == None:
                     p = point
-                elif p.meditation != point.meditation:
-                    p.meditation += float(point.meditation - p.meditation) / steps
+                elif p.meditation_avg != point.meditation_avg:
+                    p.meditation_avg += float(point.meditation_avg - p.meditation_avg) / steps
                 return p
 
             p1 = delta(p1, point1)
             p2 = delta(p2, point2)
-            fullbar = bar_from_datapoints(p1, p2)
-            print '|%s| %d\t%d' % (fullbar, int(p1.meditation*100), int(p2.meditation*100))
+            percentage = percentage_from_values(p1.meditation_avg, p2.meditation_avg)
+            fullbar = bar_from_percentage(percentage)
+            print '%s' % fullbar
+            print '%.4f %.4f %.4f -> %.4f' % (p1.meditation_avg, p2.meditation_avg, 
+                delta_needed_to_win(), percentage)
             opcClient.put_pixels(pixels_from_bar(fullbar))
-        time.sleep(1.0/fps)
+
+            #winner must 'win' for a certain amount of time to be legit
+            if percentage == 0.0 or percentage == 1.0:
+                potential_winner = percentage
+                if win_time == None:
+                    win_time = time.time()
+            else:
+                potential_winner = None
+            if potential_winner != None and (time.time() - win_time) > TIME_AT_MAX:
+                winning_player = 'one' if potential_winner == 0.0 else 'two'
+                print 'winner is player %s' % winning_player
+                sys.exit(0)
+
+        time.sleep(1.0 / fps)
   
