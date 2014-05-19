7c4eb02378b7d218e8d28878c76e4f2df953564b
Carl Hill-Popper <chillpop@gmail.com>
2014-05-18 22:41:20 -0500

    Updated led_plaything to test new effects

--- led_plaything.py
+++ led_plaything.py
@@ -10,15 +10,45 @@ from effects.base import *
 from effects.drifters import *
 from effects.impulses import *
 from parameters import SharedParameters
+from game_effects import *
+import colorsys
 
+class SimpleController(AnimationController):
+
+    def drawFrame(self):
+        """Render a frame and send it to the OPC server"""
+        self.advanceTime()
+        self.checkInput()
+        frame = numpy.zeros((self.params.num_pixels, 3))
+        self.renderer_low.render(self.params, frame)
+        
+        # adjust brightness
+        numpy.multiply(frame, self.params.brightness, frame)
+
+        self.frameToHardwareFormat(frame)
+        self.opc.putPixels(0, frame)
             
 if __name__ == '__main__':
-    playlist = Playlist([[
-        # TimedColorDrifterLayer([(0,1,0), (0,1,1), (1,0,1)], 5), 
-        # ResponsiveColorDrifterLayer([(1,0,0), (0,0,1)]), 
-        # SnowstormLayer(),
-        TechnicolorSnowstormLayer(),
-    ]])
+    def random_color():
+        return colorsys.hsv_to_rgb(random.random(),1,1)
+
+    playlist = Playlist([
+        [ WhiteOutLayer() ],
+        [ RGBLayer() ],
+        [ SnowstormLayer() ],
+        [ TechnicolorSnowstormLayer() ],
+        [ ColorSnowstormLayer(random_color()) ],
+        [ PulsingColorLayer(random_color()) ],
+        [ PulsingColorLayer(random_color(), speed=2.0) ],
+        [ VariableColorLayer(random_color(), speed=0) ],
+        [ VariableColorLayer(random_color(), speed=5, length_of_peak=10) ],
+        [ VariableColorLayer(random_color(), speed=1) ],
+        [ VariableColorLayer(random_color(), speed=-1) ],
+        [ VariableColorLayer(random_color(), speed=-2.5, length_of_peak=30, minimum=0.5) ],
+        [ ColorSnowstormLayer(random_color()), 
+            VariableColorLayer(random_color(), minimum=0.2), 
+            PulsingMultiplierLayer()],
+        ])
 
     test = len(sys.argv) > 1 and sys.argv[1] == 'test'
   
@@ -30,5 +60,7 @@ if __name__ == '__main__':
     shared_params.targetFrameRate = 100.0;
 
     renderer = Renderer(playlists={'all': playlist}, gamma=2.2)
-    controller = AnimationController(renderer=renderer, params=shared_params, server=ip_address)
+    controller = SimpleController(renderer_low=renderer, params=shared_params, server=ip_address, 
+        game_object=None, renderer_high=None)
     controller.drawingLoop()
+
