f8e4eafbd4066ceec93a41d4221ff4f472f5b3b8
Carl Hill-Popper <chillpop@gmail.com>
2014-05-19 22:54:21 -0500

    OscillatingTwoColorLayer and OscillatingSpeedResponsiveTwoColorLayer
    Oscillates between two colors spatially, with optional phase shifting animation.
    
    The speed responsive version isn't working well yet.

--- game_effects.py
+++ game_effects.py
@@ -15,8 +15,11 @@ def generate_player_renderer(params, color, inverse=False):
     speed = -1.0
     if inverse:
         speed *= -1.0
+    hsv = colorsys.rgb_to_hsv(color[0], color[1], color[2])
+    similar_color = colorsys.hsv_to_rgb(hsv[0] + 0.2, hsv[1], hsv[2])
     regular_play = Playlist([
-    	[VariableColorLayer(color, speed=speed)]
+        [OscillatingSpeedResponsiveTwoColorLayer(color, similar_color, inverse=inverse)]
+    	#[OscillatingColorLayer(color, speed=speed)]
         # [PulsingColorLayer(color, 1.0, 0.5, 1.0, random.random())]
         ])
 
@@ -71,9 +74,9 @@ class PulsingMultiplierLayer(EffectLayer):
 		scale = oscillating_value(params.time, self.speed, self.minimum, self.span, self.phase_shift)
 		numpy.multiply(frame, scale, frame)
 
-class VariableColorLayer(ColorLayer):
+class OscillatingColorLayer(ColorLayer):
 	def __init__(self, color, length_of_peak=20, speed=1.0, minimum=0.8, maximum=1.0):
-		super(VariableColorLayer,self).__init__(color)
+		super(OscillatingColorLayer,self).__init__(color)
 		self.color_frame = None
 		self.length_of_peak = length_of_peak
 		self.speed = speed
@@ -91,8 +94,45 @@ class VariableColorLayer(ColorLayer):
 		return temp_frame
 
 	def render(self, params, frame):
-
 		self.color_frame = self.create_color_frame(frame, params.time * self.speed)
-
-		# numpy.roll(self.color_frame, int())
 		numpy.add(frame, self.color_frame, frame)
+
+class OscillatingTwoColorLayer(EffectLayer):
+    def __init__(self, color_one, color_two, length_of_peak=20, speed=1.0):
+        self.color_one = color_one
+        self.color_two = color_two
+        self.color_frame = None
+        self.length_of_peak = length_of_peak
+        self.speed = speed
+
+    def create_two_color_frame(self, frame, phase_shift=0.0):
+        temp_frame = numpy.zeros_like(frame)
+        step = 1.0 / self.length_of_peak
+        for x in range(0, len(frame)):
+            scale = oscillating_value(x, step, 0.0, 1.0, phase_shift)
+            color_value_one = numpy.multiply(self.color_one, scale)
+            color_value_two = numpy.multiply(self.color_two, 1.0 - scale)
+            color_value = numpy.add(color_value_one, color_value_two)
+            numpy.add(temp_frame[x], color_value, temp_frame[x])
+        return temp_frame
+
+    def render(self, params, frame):
+        self.color_frame = self.create_two_color_frame(frame, params.time * self.speed)
+        numpy.add(frame, self.color_frame, frame)
+
+
+class OscillatingSpeedResponsiveTwoColorLayer(OscillatingTwoColorLayer):
+    def __init__(self, color_one, color_two, length_of_peak=20, speed_low=3.0, speed_high=1.0, inverse=False):
+        super(OscillatingSpeedResponsiveTwoColorLayer,self).__init__(
+            color_one, color_two, length_of_peak, 1.0)
+        self.speed_low = speed_high
+        self.span = speed_low - speed_high
+        if inverse:
+            self.speed_low = speed_low
+            self.span = speed_high - speed_low
+
+    def render(self, params, frame):
+        # percentage = 
+        alpha = round(params.percentage, 2)
+        self.speed = self.speed_low + alpha * self.span
+        super(OscillatingSpeedResponsiveTwoColorLayer,self).render(params, frame)
